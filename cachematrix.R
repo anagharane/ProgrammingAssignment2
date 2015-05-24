0e253b8a9c9487a4cef530e7962c15be29554757
anagharane <anagha.rane17@gmail.com>
2015-05-25 04:32:47 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Creates a special 'matrix' that can cache its inverse.
+##Computes and retrieves the inverse from the cache.
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(matr = matrix()) {
+          ## matr : A square invertible matrix
+          ## List containing functions to:
+          ##                        1. set the matrix
+          ##                        2. get the matrix       
+          ##                        3. set the inverse  
+          ##                        4. get the inverse
 
+  inversed <- NULL         
+  set <- function(y) { 
+          ## use '<<-' assign a value to an object in an environment that is different from the current environment.
+    matr <<- y
+    inversed <<- NULL
+  }
+  
+  get <- function() matr      
+setinverse <- function(inverse) inversed <<- inverse  
+getinverse <- function() inversed                   
+list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matr, ...) {
+          ## @matr : output of makeCacheMatrix()
+          ## return: inverse of the original matrix input 
+          
+  inversed <- matr$getinverse()
+   
+          # if the inverse has already been calculated
+   if(!is.null(inversed)) {  
+          # gets it from the cache and skips the computation.
+          
+          
+     message("getting cached data")
+     return(inversed)
+   }
+   
+          # otherwise, calculates the inverse
+   data <- matr$get()         
+   inversed <- solve(data)
+   
+          # sets the value of the inverse in the cache.
+   matr$setinverse(inversed)  
+   
+   inversed
 }
+
+## Example:
+# matr <- makeCacheMatrix(matrix(1:4, 2, 2))
+#> cacheSolve(matr)
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+
+# matr$set(matrix(5:8, 2, 2))
+# cacheSolve(matr)
+#     [,1] [,2]
+#[1,]   -4  3.5
+#[2,]    3 -2.5
